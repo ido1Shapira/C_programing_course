fff18a72041eec8861ed5ceb23511482fbe2ab49
Ido <idshapira051@gmail.com>
2019-11-14 10:50:47 +0200

    update main and power files

--- power.c
+++ power.c
@@ -1,20 +1,18 @@
-double Exp (int x){
-  double e = 1; // initialize sum of series
-  for (int i = 10; i > 0; --i ) {
-    e = 1 + x * e / i;
-  }
-  double ans = 1;
-  while (x > 0) {
-    ans = e * ans;
-    x--;
-  }
-  return ans;
-}
 double Pow (double x,int y){
   double ans = 1;
+  if(y<0){
+    y= -1 * y;
+  }
   while (y > 0){
     ans = x * ans;
     y--;
   }
-  return ans;
+  return (y<0)? 1/ans : ans;
+}
+double Exp (int x){
+  double e = 1;
+  for (int i = 10; i > 0; --i ) {
+    e = 1 + x * e / i;
+  }
+  return Pow(e,x);
 }
