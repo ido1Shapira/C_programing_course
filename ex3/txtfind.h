bbdd494134a031ce7383ae6cf63680403e7a5ae1
Noa <noa.aizer@gmail.com>
2019-12-09 11:51:09 +0200

    uploading h files

---
+++ ex3/txtfind.h
@@ -0,0 +1,6 @@
+int getline(char []s);
+int getword (char []w);
+int substring(char *str1 ,char *str2);
+int similar(char *s, char *t, int n);
+void print_lines(char *s);
+void print_similar_words(char *str)
