82a20e0e328f8a12227b2851dd5c131d98d2cdc5
Ido Shapira <46196656+ido1Shapira@users.noreply.github.com>
2020-10-18 21:09:46 +0300

    Delete txtfind.h

--- txtfind.h
+++
@@ -1,9 +0,0 @@
-#define LINE 256
-#define WORD 30
-
-int getLine(char []);
-int getword (char []);
-int substring(char * ,char *);
-int similar(char *, char *, int);
-void print_lines(char *);
-void print_similar_words(char *);
